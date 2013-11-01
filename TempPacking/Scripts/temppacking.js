8e4271201779f733d642158b122ac6451ac7d6f9
gabor-at-reed <gabor.farkas@reedonline.co.uk>
2013-11-01 16:01:48 +0000

    bg

--- TempPacking/Scripts/temppacking.js
+++ TempPacking/Scripts/temppacking.js
@@ -20,6 +20,7 @@
             skills: ko.observableArray([]),
             destinations: ko.observableArray([]),
             currentDestination: ko.observable(0),
+            currentBg: ko.observable('01'),
             showResults: ko.observable(false),
             addSkill: function () {
                 if (thisObj.elements.skill.val() != "") {
@@ -54,6 +55,16 @@
         
         initCountryLoad(thisObj);
         initSkillsLoad(thisObj);
+
+        var bgChanger = setInterval(function() {
+            if (thisObj.viewModel.currentBg() == '01') {
+                thisObj.elements.globeContainer.css("background-image", 'url("/Content/images/bg-' + thisObj.viewModel.currentBg() + '.jpg")');
+                thisObj.viewModel.currentBg('02');
+            } else {
+                thisObj.elements.globeContainer.css("background-image", 'url("/Content/images/bg-' + thisObj.viewModel.currentBg() + '.jpg")');
+                thisObj.viewModel.currentBg('01');
+            }
+        }, 6000);
 
         thisObj.viewModel.showResults.subscribe(function(newValue) {
             if (newValue) {
@@ -91,24 +102,10 @@
     };
 
     var initCountryLoad = function (thisObj) {
-        ////http://tosbourn.com/2013/08/javascript/upgrading-from-bootstraps-typeahead-to-typeahead-js/
-        ////http://twitter.github.io/typeahead.js/examples/
-
-        //thisObj.elements.countries.typeahead([{
-        //    remote: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%QUERY&sensor=false&types=(cities)&language=en_UK&key=AIzaSyDftozbK86qlycMZzBFBuZ99fP10hV-edY",
-        //    limit: 5
-        //}]);
-
-        //thisObj.elements.countries.bind('typeahead:selected', function (obj, datum) {
-        //    value(datum.value);
-        //});
-
         var autocomplete = new google.maps.places.Autocomplete(document.getElementById('country'));
         google.maps.event.addListener(autocomplete, 'place_changed', function () {
             console.log(thisObj.elements.countries.val());
         });
-
-      
     };
     
     var initSkillsLoad = function (thisObj) {
