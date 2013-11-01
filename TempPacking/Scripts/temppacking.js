cdf5082c60d9d0c0a64489fca45ac4fa00274eae
Shamsuddin Pabani <shamsuddin.pabani@reedonline.co.uk>
2013-11-01 14:26:38 +0000

    api

--- TempPacking/Scripts/temppacking.js
+++ TempPacking/Scripts/temppacking.js
@@ -45,16 +45,21 @@
     };
 
     var initCountryLoad = function (thisObj) {
-        //http://tosbourn.com/2013/08/javascript/upgrading-from-bootstraps-typeahead-to-typeahead-js/
-        //http://twitter.github.io/typeahead.js/examples/
+        ////http://tosbourn.com/2013/08/javascript/upgrading-from-bootstraps-typeahead-to-typeahead-js/
+        ////http://twitter.github.io/typeahead.js/examples/
 
-        thisObj.elements.countries.typeahead([{
-            remote: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%QUERY&sensor=false&types=(cities)&language=en_UK&key=AIzaSyD3JtusVUU4QaA_hKXRJZh48ovdfAEyYNc",
-            limit: 5
-        }]);
+        //thisObj.elements.countries.typeahead([{
+        //    remote: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%QUERY&sensor=false&types=(cities)&language=en_UK&key=AIzaSyDftozbK86qlycMZzBFBuZ99fP10hV-edY",
+        //    limit: 5
+        //}]);
 
-        thisObj.elements.countries.bind('typeahead:selected', function (obj, datum) {
-            value(datum.value);
+        //thisObj.elements.countries.bind('typeahead:selected', function (obj, datum) {
+        //    value(datum.value);
+        //});
+
+        var autocomplete = new google.maps.places.Autocomplete(document.getElementById('country'));
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            console.log(thisObj.elements.countries.val());
         });
 
       
