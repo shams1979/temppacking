0c73dd5377c49f6b2e58948a1ea7135c59d4cd32
unknown <gabor.farkas@online-nova101.reedonline.local>
2013-11-01 10:07:41 +0000

    js file

---
+++ TempPacking/Scripts/temppacking.js
@@ -0,0 +1,21 @@
+﻿temppacking.index = function() {
+    var init = function(options) {
+        this.options = options;
+
+        var elements = {
+            
+        };
+        var viewModel = {
+            
+        };
+
+        this.elements = elements;
+        this.viewModel = viewModel;
+
+    };
+    
+    return {
+        init: init
+    };
+
+}();
