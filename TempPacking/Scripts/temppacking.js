d661010a8ac83328a82fb605267a650df9fd3a1b
gabor-at-reed <gabor.farkas@reedonline.co.uk>
2013-11-01 11:41:55 +0000

    temp

--- TempPacking/Scripts/temppacking.js
+++ TempPacking/Scripts/temppacking.js
@@ -1,17 +1,43 @@
 ﻿temppacking = function() {
     var init = function(options) {
         this.options = options;
+        var thisObj = this;
+        
 
         var elements = {
-            
+            globeContainer: $("#globe"),
+            optionsContainer: $("#tripOptions"),
+            countries: $("#countries")
         };
         var viewModel = {
             
         };
 
+
         this.elements = elements;
         this.viewModel = viewModel;
 
+        initCountryLoad(thisObj);
+
+    };
+
+    var initCountryLoad = function (thisObj) {
+        //http://tosbourn.com/2013/08/javascript/upgrading-from-bootstraps-typeahead-to-typeahead-js/
+        //http://twitter.github.io/typeahead.js/examples/
+        thisObj.elements.countries.typeahead({
+            //source: function (query, process) {
+            //    return $.get('/my_search_url', { query: query }, function (data) {
+            //        return process(data.options);
+            //    });
+            //}
+            name: 'countries',
+            prefetch: '../data/countries.json',                                         
+            limit: 10
+        });
+
+        thisObj.elements.countries.bind('typeahead:selected', function(obj, datum) {
+            alert(JSON.stringify(obj)); // object
+        });
     };
     
     return {
