86f6528909b5a66fdf6ea3a545bdedaac0e52cd8
Shamsuddin Pabani <shamsuddin.pabani@reedonline.co.uk>
2013-11-01 12:23:40 +0000

    dffd

--- TempPacking/Scripts/temppacking.js
+++ TempPacking/Scripts/temppacking.js
@@ -7,7 +7,8 @@
         var elements = {
             globeContainer: $("#globe"),
             optionsContainer: $("#tripOptions"),
-            countries: $("#countries")
+            countries: $("#country"),
+            skills: $('#skill')
         };
         var viewModel = {
             
@@ -18,26 +19,38 @@
         this.viewModel = viewModel;
 
         initCountryLoad(thisObj);
+        initSkillsLoad(thisObj);
 
     };
 
     var initCountryLoad = function (thisObj) {
         //http://tosbourn.com/2013/08/javascript/upgrading-from-bootstraps-typeahead-to-typeahead-js/
         //http://twitter.github.io/typeahead.js/examples/
-        thisObj.elements.countries.typeahead({
-            source: function (query, process) {
-                return $.get('/home/FindSkill', { query: query }, function (data) {
-                    return process(data.options);
-                });
-            }
-            //name: 'countries',
-            //prefetch: '../data/countries.json',                                         
-            //limit: 10
+        
+        thisObj.elements.countries.typeahead([{
+            remote: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%QUERY&sensor=false&types=(cities)&language=en_UK&key=AIzaSyD3JtusVUU4QaA_hKXRJZh48ovdfAEyYNc",
+            limit: 5
+        }]);
+
+        thisObj.elements.countries.bind('typeahead:selected', function (obj, datum) {
+            value(datum.value);
         });
 
-        thisObj.elements.countries.bind('typeahead:selected', function(obj, datum) {
-            alert(JSON.stringify(obj)); // object
+      
+    };
+    
+    var initSkillsLoad = function (thisObj) {
+       
+        thisObj.elements.skills.typeahead([{
+            remote: "/home/findskill?query=%QUERY",
+            limit: 5
+        }]);
+
+        thisObj.elements.skills.bind('typeahead:selected', function (obj, datum) {
+            value(datum);
         });
+
+
     };
     
     return {
